ca787da3a1415c350ea3dbfdb86c94a485bedc72
Laura Lewis <lvlewis8@gmail.com>
2018-10-18 14:17:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,11 @@ def turn_count(board)
   end
     return counter
 end
+
+def current_player(board)
+  if turn_count % 2 == 0
+    character = "X"
+  elsif turn_count % 2 ==1
+    character = "O"
+  end
+end
