18235d6e3005b15cd70684d5b1e8fa4c54893365
Laura Lewis <lvlewis8@gmail.com>
2018-10-18 14:23:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,9 @@ end
 
 def current_player(board)
   turns = turn_count(board)
-  if turns % 2 == 0
-    return "O"
-  else
+  if turns.even? = true
     return "X"
+  else
+    return "O"
   end
 end
